bf4beba1341b041f3fc3a35074adf3e65839c4e3
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2024-01-19 03:19:09 +0800

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,31 +13,36 @@ export default function Navbar() {
   }
   return (
     <>
-        <div className="navContainer" >
-            <h1>TraPinas</h1>
-            <div className="navLinks">
-                <a href="#home">Home</a>
-                <a href="#service">About</a>
-                <a href="#destination">Destinations</a>
-                <a href="">Packages</a>
-                <a href="">Blog</a>
-                <a href="">Contact Us</a>
+        <div className="w-11/12 ml-10 mt-10 flex justify-center items-center space-x-64 h-12 md:space-x-24 sm:space-x-32" >
+            <h1 className='text-4xl font-bold text-gradient'>TraPinas</h1>
+            <div className="flex text-xl gap-6 sm:hidden">
+                <a href="#home" className='nav'>Home</a>
+                <a href="#service" className='nav'>About</a>
+                <a href="#destination" className='nav'>Destinations</a>
+                <a href="" className='nav'>Packages</a>
+                <a href="" className='nav'>Blog</a>
+                <a href="" className='nav'>Contact</a>
             </div>
-            <div className="menu">
-              <FontAwesomeIcon icon={faBars} style={{"--fa-primary-color": "#f85e98", "--fa-secondary-color": "#5d56c6", width: 90, height: 40}} className='burgerMenu' onClick={handleClose}/>
-              <div className="menuContent" style={close ? { display: "block" } : {display:'none'}}>
-                <FontAwesomeIcon icon={faX} style={{color: "#5d50c6",}} className='navCloseBtn' onClick={handleClose}/>
-                <div className="links">
-                  <a href="#home">Home</a>
-                  <a href="#service">About</a>
-                  <a href="#destination">Destinations</a>
-                  <a href="">Packages</a>
-                  <a href="">Blog</a>
-                  <a href="">Contact Us</a>
-                </div>
+            <div className='hidden sm:block md:block'>
+            <div className={`absolute w-screen h-80 left-0 bg-fuchsia-400 mt-2 ${close ? 'sm:block md:block': 'hidden'}`}>
+              <FontAwesomeIcon icon={faX} 
+              style={{color: "#fafcff", width:40, height:30, 
+              marginTop:20, position:'absolute', right:0, marginRight: 
+              20, cursor:'pointer'}} 
+              onClick={handleClose}/>
+              <div className='mt-12 w-screen text-white 
+              grid place-items-center gap-4'>
+                <a href="#home" className='nav'>Home</a>
+                <a href="#service" className='nav'>About</a>
+                <a href="#destination" className='nav'>Destinations</a>
+                <a href="" className='nav'>Packages</a>
+                <a href="" className='nav'>Blog</a>
+                <a href="" className='nav'>Contact</a>
               </div>
             </div>
-        </div>
+            <FontAwesomeIcon icon={faBars} style={{"--fa-primary-color": "#ce0de7", "--fa-secondary-color": "#94369b", width:40, height:40, marginRight:40, cursor:'pointer'}} onClick={handleClose}/>
+            </div>
+          </div>
     </>
   )
 }
