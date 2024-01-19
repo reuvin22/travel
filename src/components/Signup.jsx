1b385adc6140d38277020f93bddec63702f25b41
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2024-01-19 09:17:32 +0800

    Update

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -10,7 +10,7 @@ export default function Signup() {
                     <h1 className='font-bold text-2xl lg:text-4xl xl:text-4xl'>Sign up to our newsletter</h1>
                     <p className='text-md text-center mt-5 lg:w-4/5 xl:w-4/5 md:w-[80%]'>Lorem ipsum dolor sit amet consectetur. Egestas et feugiat purus enim facilisi nunc blandit nullam.</p>
                     <div className='flex justify-center items-center w-full'>
-                        <input type="text" name="signup" id="signupInput" placeholder='Enter your Email Address' className='border-2 rounded-2xl h-12 px-3 mt-5 '/>
+                        <input type="text" name="signup" id="signupInput" placeholder='Enter your Email Address' className='border-2 rounded-2xl h-12 px-3 mt-5 sm:w-[60%]'/>
                         <img src={Send} alt="Send" className='w-14 relative mt-8 cursor-pointer' />
                     </div>
                 </div>
